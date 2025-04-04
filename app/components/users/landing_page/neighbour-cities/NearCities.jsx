721fca126ccf441cfad2a3a67e501a978acf789c
jai <jaisoni@startbitsolutions.com>
2025-04-04 17:53:13 +0530

    UI

---
+++ app/components/users/landing_page/neighbour-cities/NearCities.jsx
@@ -0,0 +1,20 @@
+
+const NearCities = () => {
+    return (
+        <div className="  ">
+            <div className="text-center mt-[50px] ">
+
+                <h1 className="text-[1.8rem] font-[700] tracking-wide mb-2 ">
+                    Explore The Neighborhoods
+                </h1>
+                <p className="text-gray-500 text-[0.8rem]  font-[400] tracking-[1px]   ">Find your dream apartment with our listing </p>
+                
+              
+
+                
+            </div>
+        </div>
+    )
+}
+
+export default NearCities
--- app/routes/_index.jsx
+++ app/routes/_index.jsx
@@ -2,13 +2,15 @@ import NavigationBar from "../components/users/landing_page/navbar_component/Nav
 import MainBanner from "../components/users/landing_page/main-banner/MainBanner"
 import FlatListing from "../components/users/landing_page/flats-listing/FlatListing";
 import Footer from "../components/users/landing_page/footer/Footer";
+import NearCities from "../components/users/landing_page/neighbour-cities/NearCities";
 export default function Index() {
   return (
     <div className=" ">
       <NavigationBar>
         <MainBanner />
-        <FlatListing/>
-        <Footer/>
+        <FlatListing />
+        <NearCities />
+        <Footer />
       </NavigationBar>
     </div>
   );
