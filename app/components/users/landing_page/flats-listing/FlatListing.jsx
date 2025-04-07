d9a3d818f135a73901fbe45e1a5a29cc93452b47
jai <jaisoni@startbitsolutions.com>
2025-04-07 18:36:47 +0530

    UI change

--- app/components/users/landing_page/flats-listing/FlatListing.jsx
+++ app/components/users/landing_page/flats-listing/FlatListing.jsx
@@ -16,7 +16,7 @@ const FlatListing = () => {
                     Today&apos;s Luxury Listings
                 </h1>
                 <p className="text-gray-500 text-[0.8rem]  font-[400] tracking-[1px]   ">Thousands of luxury flats and rooms enthusiasts just like you visit our website </p>
-                <div className="container mx-auto px-4 grid grid-cols-1 sm:grid-cols-2   lg:grid-cols-3 gap-8 mt-12 mb-5">
+                <div className="container lg:w-[75%] md:w-[85%] sm:w-[90%] mx-auto px-4 grid grid-cols-1 sm:grid-cols-2   lg:grid-cols-3 gap-8 mt-12 mb-5">
                     {images.map((item, key) => (
                         <CardView item={item} key={key} />
                     ))}
